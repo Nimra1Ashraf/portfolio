ee02ed44b50e0be81ba6f70e2cdd55d56840604a
Nimra1Ashraf <arslanashraf3377@gmail.com>
2026-01-14 17:46:49 +0500

    my portfolio

---
+++ portfolio.js
@@ -0,0 +1,57 @@
+const links = document.querySelectorAll('a');
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+})
+
+function openGitHubCalculator() {
+    window.open(
+        "https://github.com/Nimra1Ashraf/calculator-web",
+        "_blank"
+    );
+}
+
+function openLinkedInCalculator() {
+    window.open(
+        "https://www.linkedin.com/in/nimra-ashraf-095079339/overlay/projects/739749288/multiple-media-viewer?profileId=ACoAAFTnxDEBrwleQK0SHfJaUmTC2R65mTN1xT4&treasuryMediaId=1768118792505&type=LINK&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BGyMIMfpRSkGv8TbF9BAS4g%3D%3D", "_blank"
+    );
+}
+
+function openGithubgallery() {
+    window.open(
+        "https://github.com/Nimra1Ashraf/Web-Gallery",
+        "_blank"
+    );
+}
+
+function openLinkedIngallery() {
+    window.open(
+        "https://www.linkedin.com/in/nimra-ashraf-095079339/overlay/projects/740233277/multiple-media-viewer?profileId=ACoAAFTnxDEBrwleQK0SHfJaUmTC2R65mTN1xT4&treasuryMediaId=1768119276534&type=LINK&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BGyMIMfpRSkGv8TbF9BAS4g%3D%3D", "_blank"
+    );
+}
+
+function openGitHubCertificate() {
+    window.open(
+        "https://github.com/Nimra1Ashraf/AI-Career-Empowerment-Certificate",
+        "_blank"
+    );
+}
+function openLinkedInCertificate() {
+    window.open(
+        "https://www.linkedin.com/posts/nimra-ashraf-095079339_ai-and-career-empowerment-activity-7415977063154823169-FZVq?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFTnxDEBrwleQK0SHfJaUmTC2R65mTN1xT4",
+        "_blank"
+    );
+}
+function openGitHubResume() {
+    window.open(
+        "https://github.com/Nimra1Ashraf/Resume",
+        "_blank"
+    );
+}
+function openLinkedInResume() {
+    window.open(
+        "https://www.linkedin.com/posts/nimra-ashraf-095079339_freshgraduate-itcareers-opentowork-below-activity-7415975465527443458-zL5I?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFTnxDEBrwleQK0SHfJaUmTC2R65mTN1xT4",
+        "_blank"
+    );
+}
